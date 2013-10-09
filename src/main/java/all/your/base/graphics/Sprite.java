e8e90e68a6fbf52645bb53b8e95e56773834b21c
Simon Thoresen Hult <simon@yahoo-inc.com>
2013-10-09 07:54:56 +0200

    add draft of graphics classes

---
+++ src/main/java/all/your/base/graphics/Sprite.java
@@ -0,0 +1,8 @@
+package all.your.base.graphics;
+
+/**
+ * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
+ */
+public class Sprite {
+
+}
---
+++ src/main/java/all/your/base/graphics/SpriteBlueprint.java
@@ -0,0 +1,11 @@
+package all.your.base.graphics;
+
+/**
+ * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
+ */
+public class SpriteBlueprint {
+
+    public static class Builder {
+
+    }
+}
