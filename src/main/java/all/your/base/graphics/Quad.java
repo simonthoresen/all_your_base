b6a077110c8afec4e76b9d55c7c8ba7daf13e47c
Simon Thoresen Hult <simon@yahoo-inc.com>
2013-10-14 07:35:48 +0200

    add quad

---
+++ src/main/java/all/your/base/graphics/Quad.java
@@ -0,0 +1,10 @@
+package all.your.base.graphics;
+
+/**
+ * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
+ */
+public class Quad {
+
+    private Texture texture;
+    private int x, y, width, height;
+}
