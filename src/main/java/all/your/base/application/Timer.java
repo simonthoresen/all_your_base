3ff1fa5178127478f67e58bed7d3d1ddb3738940
Simon Thoresen Hult <simon@yahoo-inc.com>
2013-10-18 13:07:36 +0200

    add a timer class

---
+++ src/main/java/all/your/base/application/Timer.java
@@ -0,0 +1,9 @@
+package all.your.base.application;
+
+/**
+ * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
+ */
+public interface Timer {
+
+    public long currentTimeMillis();
+}
---
+++ src/test/java/all/your/base/application/SystemTimerTest.java
@@ -0,0 +1,22 @@
+package all.your.base.application;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
+ */
+public class SystemTimerTest {
+
+    @Test
+    public void requireThatSystemTimerIsATimer() {
+        assertTrue(Timer.class.isAssignableFrom(SystemTimer.class));
+    }
+
+    @Test
+    public void requireThatSystemTimerReturnsSystemTime() {
+        assertEquals(System.currentTimeMillis(), SystemTimer.INSTANCE.currentTimeMillis(), 100);
+    }
+}
