5b3cb230d3990678443bbdfb230aad309e2d6950
Simon Thoresen Hult <simon@yahoo-inc.com>
2013-10-18 23:02:22 +0200

    process event queue

--- src/test/java/all/your/base/application/SimpleApplicationState.java
+++ src/test/java/all/your/base/application/SimpleApplicationState.java
@@ -2,6 +2,8 @@ package all.your.base.application;
 
 import all.your.base.graphics.Surface;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
  */
@@ -10,7 +12,8 @@ public class SimpleApplicationState implements ApplicationState {
     private volatile boolean shutdown = false;
 
     @Override
-    public void update(ApplicationManager appManager) {
+    public void update(ApplicationManager appManager) throws InterruptedException {
+        appManager.processEventQueue(100, TimeUnit.MILLISECONDS);
         if (shutdown) {
             appManager.shutdown();
         }
