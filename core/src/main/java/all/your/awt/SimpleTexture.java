22b97d0fb8c31d9f9f364206395b524cfbb30122
Simon Thoresen Hult <simon@hult-thoresen.com>
2013-10-29 21:30:06 +0100

    add test for SimpleTexture

--- core/src/main/java/all/your/awt/SimpleTexture.java
+++ core/src/main/java/all/your/awt/SimpleTexture.java
@@ -2,7 +2,6 @@ package all.your.awt;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 /**
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
@@ -18,9 +17,5 @@ public class SimpleTexture implements Texture {
     @Override
     public void paint(Graphics2D g, int x, int y, int width, int height) {
         g.drawImage(image, x, y, width, height, null);
-    }
-
-    public static SimpleTexture fromFile(String fileName) throws IOException {
-        return new SimpleTexture(BufferedImages.fromFile(fileName));
     }
 }
--- core/src/test/java/all/your/awt/SimpleTextureTest.java
+++ core/src/test/java/all/your/awt/SimpleTextureTest.java
@@ -1,8 +1,49 @@
 package all.your.awt;
 
+import org.junit.Test;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
  */
 public class SimpleTextureTest {
 
+    @Test
+    public void requireThatTextureIsDrawnToGivenRectangle() {
+        BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = image.createGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 100, 100);
+        new SimpleTexture(BufferedImages.newSquareGrid(2, 2, new Color[][] {
+                { Color.RED, Color.ORANGE },
+                { Color.YELLOW, Color.GREEN }
+        })).paint(g, 25, 25, 50, 50);
+        g.dispose();
+
+        for (int y = 0; y < 100; ++y) {
+            for (int x = 0; x < 100; ++x) {
+                Color actual = new Color(image.getRGB(x, y));
+                Color expected = Color.BLACK;
+                if (y >= 25 && y < 50) {
+                    if (x >= 25 && x < 50) {
+                        expected = Color.RED;
+                    } else if (x >= 50 && x < 75) {
+                        expected = Color.ORANGE;
+                    }
+                } else if (y >= 50 && y < 75) {
+                    if (x >= 25 && x < 50) {
+                        expected = Color.YELLOW;
+                    } else if (x >= 50 && x < 75) {
+                        expected = Color.GREEN;
+                    }
+                }
+                assertEquals("(" + x + "," + y + ")", expected, actual);
+            }
+        }
+    }
 }
