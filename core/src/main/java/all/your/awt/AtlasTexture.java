c67556df788229280978d56725d2ee7095fca300
Simon Thoresen Hult <simon@hult-thoresen.com>
2013-10-29 21:49:28 +0100

    add preconditions to constructor

--- core/src/main/java/all/your/awt/AtlasTexture.java
+++ core/src/main/java/all/your/awt/AtlasTexture.java
@@ -1,7 +1,10 @@
 package all.your.awt;
 
+import com.google.common.base.Preconditions;
+
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.Objects;
 
 /**
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
@@ -12,6 +15,11 @@ public class AtlasTexture implements Texture {
     private final int x, y, width, height;
 
     AtlasTexture(BufferedImage atlas, int x, int y, int width, int height) {
+        Objects.requireNonNull(atlas, "atlas");
+        Preconditions.checkArgument(x > 0, "x <= 0");
+        Preconditions.checkArgument(y > 0, "y <= 0");
+        Preconditions.checkArgument(width > 0, "width <= 0");
+        Preconditions.checkArgument(height > 0, "height <= 0");
         this.atlas = atlas;
         this.x = x;
         this.y = y;
