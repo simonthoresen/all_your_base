2e2c81e3770d2b50456e45cc7ddccd1d5d4fcba9
Simon Thoresen Hult <simon@hult-thoresen.com>
2013-10-29 21:29:50 +0100

    add newSquaredGrid() factory method

--- core/src/main/java/all/your/awt/BufferedImages.java
+++ core/src/main/java/all/your/awt/BufferedImages.java
@@ -1,6 +1,10 @@
 package all.your.awt;
 
+import com.google.common.base.Preconditions;
+
 import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,6 +15,23 @@ import java.util.Objects;
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
  */
 public class BufferedImages {
+
+    public static BufferedImage newSquareGrid(int squareWidth, int squareHeight, Color[][] squares) {
+        Preconditions.checkArgument(squareWidth > 0, "squareWidth <= 0");
+        Preconditions.checkArgument(squareHeight > 0, "squareHeight <= 0");
+        Objects.requireNonNull(squares, "squares");
+        BufferedImage image = new BufferedImage(squareWidth * squares[0].length, squareHeight * squares.length,
+                                                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = image.createGraphics();
+        for (int row = 0; row < squares.length; ++row) {
+            for (int col = 0; col < squares[row].length; ++col) {
+                g.setColor(squares[row][col]);
+                g.fillRect(squareWidth * col, squareHeight * row, squareWidth, squareHeight);
+            }
+        }
+        g.dispose();
+        return image;
+    }
 
     public static BufferedImage fromFile(String fileName) throws IOException {
         Objects.requireNonNull(fileName, "fileName");
--- core/src/test/java/all/your/awt/BufferedImagesTest.java
+++ core/src/test/java/all/your/awt/BufferedImagesTest.java
@@ -1,11 +1,17 @@
 package all.your.awt;
 
+import all.your.swing.AbstractApplicationState;
+import all.your.swing.ApplicationBuilder;
+import all.your.swing.Surface;
 import org.junit.Test;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
@@ -13,6 +19,25 @@ import static org.junit.Assert.fail;
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
  */
 public class BufferedImagesTest {
+
+    @Test
+    public void requireThatSquareGridsCanBeCreated() {
+        int squareWidth = 2;
+        int squareHeight = 5;
+        Color[][] squares = new Color[][] {
+                { Color.RED, Color.YELLOW, Color.RED, Color.YELLOW },
+                { Color.YELLOW, Color.BLUE, Color.YELLOW, Color.BLUE },
+                { Color.RED, Color.YELLOW, Color.RED, Color.YELLOW },
+                { Color.YELLOW, Color.BLUE, Color.YELLOW, Color.BLUE },
+        };
+        BufferedImage image = BufferedImages.newSquareGrid(squareWidth, squareHeight, squares);
+        assertNotNull(image);
+        for (int x = 0; x < squares[0].length * squareWidth; ++x) {
+            for (int y = 0; y < squares.length * squareHeight; ++y) {
+                assertEquals(squares[y / squareHeight][x / squareWidth], new Color(image.getRGB(x, y)));
+            }
+        }
+    }
 
     @Test
     public void requireThatImageCanBeLoadedFromFile() throws IOException {
@@ -27,6 +52,24 @@ public class BufferedImagesTest {
             fail();
         } catch (FileNotFoundException e) {
 
+        }
+    }
+
+    public static void displayImage(final BufferedImage image, final int scale) {
+        try {
+            new ApplicationBuilder()
+                    .setWindowWidth(image.getWidth() * scale)
+                    .setWindowHeight(image.getHeight() * scale)
+                    .setInitialState(new AbstractApplicationState() {
+
+                        @Override
+                        public void render(Surface surface) {
+                            Graphics2D g = surface.getGraphics();
+                            g.drawImage(image, 0, 0, image.getWidth() * scale, image.getHeight() * scale, null);
+                        }
+                    }).build().run();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
