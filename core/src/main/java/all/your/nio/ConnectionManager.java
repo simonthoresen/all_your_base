0a57e30cc275e374710888d0d20d2f1af8fadc03
Simon Thoresen Hult <simon@hult-thoresen.com>
2013-10-30 01:00:07 +0100

    flesh out nio2 test

--- core/src/main/java/all/your/nio/ConnectionManager.java
+++ core/src/main/java/all/your/nio/ConnectionManager.java
@@ -16,56 +16,77 @@ import java.util.concurrent.TimeUnit;
 public class ConnectionManager {
 
     public static void main(String[] args) throws Throwable {
-        final AsynchronousChannelGroup allChannels = AsynchronousChannelGroup.withFixedThreadPool(
+        AsynchronousChannelGroup allChannels = AsynchronousChannelGroup.withFixedThreadPool(
                 Runtime.getRuntime().availableProcessors() * 2,
                 Executors.defaultThreadFactory());
-        final AsynchronousServerSocketChannel serverChannel =
-                AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(4080));
-        serverChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
-
-            @Override
-            public void completed(AsynchronousSocketChannel channel, Void attachment) {
-                // accept the next connection
-                serverChannel.accept(null, this);
-
-                // handle this connection
-                final ByteBuffer buf = ByteBuffer.allocate(1024);
-                channel.read(buf, 100, TimeUnit.MILLISECONDS, null,
-                             new CompletionHandler<Integer, Void>() {
-
-                                 @Override
-                                 public void completed(Integer result, Void attachment) {
-                                     System.out.println("read completed; " + result);
-                                 }
-
-                                 @Override
-                                 public void failed(Throwable t, Void attachment) {
-                                     t.printStackTrace();
-                                 }
-                             });
-                channel.write(ByteBuffer.wrap("Hello, world".getBytes(StandardCharsets.UTF_8)),
-                              100, TimeUnit.MILLISECONDS, null,
-                              new CompletionHandler<Integer, Void>() {
-
-                                  @Override
-                                  public void completed(Integer result, Void attachment) {
-                                      System.out.println("write completed; " + result);
-                                  }
-
-                                  @Override
-                                  public void failed(Throwable t, Void attachment) {
-                                      t.printStackTrace();
-                                  }
-                              });
-            }
-
-            @Override
-            public void failed(Throwable t, Void attachment) {
-                t.printStackTrace();
-            }
-        });
-
-
+        AsynchronousServerSocketChannel serverChannel =
+                AsynchronousServerSocketChannel.open(allChannels).bind(new InetSocketAddress(4080));
+        serverChannel.accept(serverChannel, AcceptCompletionHandler.INSTANCE);
         Thread.sleep(TimeUnit.DAYS.toMillis(1));
+    }
+
+    private static enum AcceptCompletionHandler implements CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel> {
+
+        INSTANCE;
+
+        @Override
+        public void completed(AsynchronousSocketChannel channel, AsynchronousServerSocketChannel serverChannel) {
+            System.out.println("accepted " + channel);
+
+            // accept the next connection
+            serverChannel.accept(serverChannel, this);
+
+            // handle this connection
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+            channel.read(buf, buf, ReadCompletionHandler.INSTANCE);
+            channel.write(ByteBuffer.wrap(("HTTP/1.0 200 OK\r\n" +
+                                           "Content-Length: 3\r\n" +
+                                           "\r\n" +
+                                           "foo").getBytes(StandardCharsets.UTF_8)), null,
+                          WriteCompletionHandler.INSTANCE);
+        }
+
+        @Override
+        public void failed(Throwable t, AsynchronousServerSocketChannel serverChannel) {
+            t.printStackTrace();
+        }
+    }
+
+    private static enum ReadCompletionHandler implements CompletionHandler<Integer, ByteBuffer> {
+
+        INSTANCE;
+
+        @Override
+        public void completed(Integer result, ByteBuffer buf) {
+            int numBytes = result;
+            if (numBytes < 0) {
+                System.out.println("done");
+                return;
+            }
+            buf.flip();
+            byte[] arr = new byte[buf.remaining()];
+            buf.get(arr);
+            System.out.println(new String(arr, StandardCharsets.UTF_8));
+        }
+
+        @Override
+        public void failed(Throwable t, ByteBuffer buf) {
+            t.printStackTrace();
+        }
+    }
+
+    private static enum WriteCompletionHandler implements CompletionHandler<Integer, Void> {
+
+        INSTANCE;
+
+        @Override
+        public void completed(Integer result, Void v) {
+            System.out.println("wrote " + result + " bytes");
+        }
+
+        @Override
+        public void failed(Throwable t, Void v) {
+            t.printStackTrace();
+        }
     }
 }
