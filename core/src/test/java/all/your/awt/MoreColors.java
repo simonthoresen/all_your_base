9c44ec746d6a5176609d8dbfce28975c4617ea0b
Simon Thoresen Hult <simon@hult-thoresen.com>
2013-11-08 23:49:29 +0100

    refactor symbol table

--- core/src/test/java/all/your/awt/MoreColors.java
+++ core/src/test/java/all/your/awt/MoreColors.java
@@ -3,48 +3,34 @@ package all.your.awt;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
-import java.util.Locale;
+import java.util.*;
 
 /**
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
  */
 public class MoreColors {
 
-    public static final Color[] COLORS = new Color[] {
-            new Color(0x000033),
-            new Color(0x000077),
-            new Color(0x0000BB),
-            new Color(0x0000FF),
-            new Color(0x003300),
-            new Color(0x007700),
-            new Color(0x00BB00),
-            new Color(0x00FF00),
-            new Color(0x330000),
-            new Color(0x770000),
-            new Color(0xBB0000),
-            new Color(0xFF0000),
-            new Color(0x333300),
-            new Color(0x777700),
-            new Color(0xBBBB00),
-            new Color(0xFFFF00),
-    };
     public static final Color C_ = null;
-    public static final Color C0 = COLORS[0x00];
-    public static final Color C1 = COLORS[0x01];
-    public static final Color C2 = COLORS[0x02];
-    public static final Color C3 = COLORS[0x03];
-    public static final Color C4 = COLORS[0x04];
-    public static final Color C5 = COLORS[0x05];
-    public static final Color C6 = COLORS[0x06];
-    public static final Color C7 = COLORS[0x07];
-    public static final Color C8 = COLORS[0x08];
-    public static final Color C9 = COLORS[0x09];
-    public static final Color CA = COLORS[0x0A];
-    public static final Color CB = COLORS[0x0B];
-    public static final Color CC = COLORS[0x0C];
-    public static final Color CD = COLORS[0x0D];
-    public static final Color CE = COLORS[0x0E];
-    public static final Color CF = COLORS[0x0F];
+    public static final Color C0 = new Color(0x000033);
+    public static final Color C1 = new Color(0x000077);
+    public static final Color C2 = new Color(0x0000BB);
+    public static final Color C3 = new Color(0x0000FF);
+    public static final Color C4 = new Color(0x003300);
+    public static final Color C5 = new Color(0x007700);
+    public static final Color C6 = new Color(0x00BB00);
+    public static final Color C7 = new Color(0x00FF00);
+    public static final Color C8 = new Color(0x330000);
+    public static final Color C9 = new Color(0x770000);
+    public static final Color CA = new Color(0xBB0000);
+    public static final Color CB = new Color(0xFF0000);
+    public static final Color CC = new Color(0x333300);
+    public static final Color CD = new Color(0x777700);
+    public static final Color CE = new Color(0xBBBB00);
+    public static final Color CF = new Color(0xFFFF00);
+
+    public static final List<Color> COLORS = Arrays.asList(C0, C1, C2, C3, C4, C5, C6, C7,
+                                                           C8, C9, CA, CB, CC, CD, CE, CF);
+    public static final Map<Integer, Character> SYMBOLS = newSymbolMap(COLORS);
 
     public static String render(Color[][] pixels) {
         StringBuilder out = new StringBuilder();
@@ -69,11 +55,15 @@ public class MoreColors {
     }
 
     private static char resolveSymbol(int rgb) {
-        for (int i = 0; i < COLORS.length; ++i) {
-            if (rgb == COLORS[i].getRGB()) {
-                return Character.forDigit(i, 16);
-            }
+        Character symbol = SYMBOLS.get(rgb);
+        return symbol != null ? symbol : '?';
+    }
+
+    private static Map<Integer, Character> newSymbolMap(List<Color> colors) {
+        Map<Integer, Character> out = new HashMap<>();
+        for (int i = 0, len = colors.size(); i < len; ++i) {
+            out.put(colors.get(i).getRGB(), String.format("%X", i).charAt(0));
         }
-        return '?';
+        return out;
     }
 }
