b5bbc572aabbb88100d82e28c42894e69ce9f5f4
Simon Thoresen Hult <simon@yahoo-inc.com>
2013-11-11 10:43:50 +0100

    implement main()

--- demo/src/main/java/all/your/demo/daylight/DaylightDemo.java
+++ demo/src/main/java/all/your/demo/daylight/DaylightDemo.java
@@ -1,8 +1,26 @@
 package all.your.demo.daylight;
 
+import all.your.swing.ApplicationBuilder;
+import all.your.swing.ApplicationManager;
+import all.your.swing.ApplicationState;
+import all.your.swing.Surface;
+
 /**
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
  */
-public class DaylightDemo {
+public class DaylightDemo implements ApplicationState {
 
+    public static void main(String[] args) throws Exception {
+        new ApplicationBuilder().setInitialState(new DaylightDemo()).build().run();
+    }
+
+    @Override
+    public void update(ApplicationManager appManager) throws Exception {
+
+    }
+
+    @Override
+    public void render(Surface surface) {
+
+    }
 }
