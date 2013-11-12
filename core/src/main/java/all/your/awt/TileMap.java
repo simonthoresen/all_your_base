88822bb02a666f081c390843e60e6276953eead5
Simon Thoresen Hult <simon@yahoo-inc.com>
2013-11-12 13:26:40 +0100

    static import math functions

--- core/src/main/java/all/your/awt/TileMap.java
+++ core/src/main/java/all/your/awt/TileMap.java
@@ -9,6 +9,10 @@ import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 import java.util.LinkedHashMap;
 
+import static java.lang.Math.ceil;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 /**
  * @author <a href="mailto:simon@yahoo-inc.com">Simon Thoresen Hult</a>
  */
@@ -46,8 +50,8 @@ public class TileMap {
         if (!mapRegion.intersects(bounds)) {
             return;
         }
-        Dimension tile = new Dimension((int)Math.max(1, viewport.width / mapRegion.getWidth()),
-                                       (int)Math.max(1, viewport.height / mapRegion.getHeight()));
+        Dimension tile = new Dimension((int)max(1, viewport.width / mapRegion.getWidth()),
+                                       (int)max(1, viewport.height / mapRegion.getHeight()));
         g = (Graphics2D)g.create(viewport.x, viewport.y, viewport.width, viewport.height);
         double mapX = mapRegion.getX();
         if (mapX < 0) {
@@ -59,13 +63,12 @@ public class TileMap {
             g.translate(0, -mapY * tile.height);
             mapY = 0;
         }
-        Point cursor = new Point((int)(((int)mapX - mapX) * tile.width), (int)(((int)mapY - mapY) * tile.height));
+        Point cursor = new Point((int)(((int)mapX - mapX) * tile.width),
+                                 (int)(((int)mapY - mapY) * tile.height));
         Rectangle region = new Rectangle(
                 (int)mapX, (int)mapY,
-                Math.min((int)Math.ceil((viewport.width - cursor.x) / (double)tile.width),
-                         bounds.width - (int)mapX),
-                Math.min((int)Math.ceil((viewport.height - cursor.y) / (double)tile.height),
-                         bounds.height - (int)mapY));
+                min((int)ceil((viewport.width - cursor.x) / (double)tile.width), bounds.width - (int)mapX),
+                min((int)ceil((viewport.height - cursor.y) / (double)tile.height), bounds.height - (int)mapY));
         for (MapLayer layer : layers.values()) {
             layer.paint(g, cursor, region, tile);
         }
